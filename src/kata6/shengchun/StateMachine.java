90707c62ef1a40d65448ef6774dfa2c10b1f9d55
Shengchun Ye <shengchun_ye@yahoo.com>
2010-12-06 06:50:54 -0800

    change package so it would not cause compiling error.

--- src/kata6/shengchun/StateMachine.java
+++ src/kata6/shengchun/StateMachine.java
@@ -1,4 +1,4 @@
-package stateMachine;
+
 
 import java.util.Map;
 import java.util.HashMap;
--- src/kata6/shengchun/TestStateMachine.java
+++ src/kata6/shengchun/TestStateMachine.java
@@ -1,4 +1,4 @@
-package stateMachine;
+
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
