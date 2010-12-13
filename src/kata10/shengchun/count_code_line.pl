0468cdd75d6ebc81c533e2652815da9b82d34cd2
Shengchun Ye <sye@demandtec.com>
2010-12-13 00:45:24 -0800

    Re-committing after an unsuccessful merge.

---
+++ src/kata10/shengchun/count_code_line.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+$input_file = $ARGV[0];
+
+open FILE, $input_file;
+$isMultilineComment = 0;
+$line_count = 0;
+while(<FILE>)
+{
+    $line = $_;
+    if($line =~ /^\s*$/){
+        ##blank line/white space only, skip
+        next;
+    }
+    if($line =~ /^\s*\/\/.*$/){
+        ##single comment line, skip
+        next;
+    }
+    if($line =~ /^\s*[\{\}]\s*$/ ){
+        ##only '{' or '}', skip
+        next;
+    }
+    if($line =~ /^\s*\/\*/ ){
+        ### /*  start of multiline comment
+        $isMultilineComment = 1;
+        next;
+    }
+    if($isMultilineComment == 1){
+        if($line =~ /\*\/\s*$/ ){
+            ##end multiline comment with */
+            $isMultilineComment = 0;
+        }
+        next;
+    }
+    else
+    {
+        $line_count++;
+        print "$line\n";
+    }
+
+
+}
+
+print "file $file_nale has $line_count lines of code\n";
+
+exit 0;
