64988ab25681529dcf52c7ab5bcbadc9b8e43446
Neal Wiggins <chillguy84@gmail.com>
2010-12-13 00:15:44 -0800

    Java File code line count implemented in Python

---
+++ src/kata10/nwiggins/countlines.py
@@ -0,0 +1,21 @@
+'''
+Created on Dec 12, 2010
+
+@author: nwiggins
+'''
+import re
+class CountLines(object):
+
+    def strippedMultiLineComments(self,javaCode):
+        javaCode = re.sub('\/\*.*\*\/', '', javaCode)
+        return re.sub('\/\*?([^/]|(\/\*?[^/]*)\*\/)*\*\/', '\n', javaCode,re.MULTILINE)
+    def stripOutLineComments(self,javaCode):
+        return re.sub('//.*','',javaCode)
+    def stripOutBlankLines(self,javaCode):
+        javaCode = re.sub('(\n|\r)\s*[\n\r]','\n',javaCode,re.MULTILINE)
+        return re.sub('^\s*','',javaCode,re.MULTILINE)
+    def numberOfLines(self, javaCode):
+        javaCode = self.strippedMultiLineComments(javaCode)
+        javaCode = self.stripOutLineComments(javaCode)
+        javaCode = self.stripOutBlankLines(javaCode)
+        return len(javaCode.splitlines())
---
+++ src/kata10/nwiggins/testcountlines.py
@@ -0,0 +1,77 @@
+'''
+Created on Dec 12, 2010
+
+@author: nwiggins
+'''
+import unittest
+from countlines import CountLines
+
+class Test(unittest.TestCase):
+
+
+    def testNestedMultiLine(self):
+        testaroo = CountLines()
+        newString = testaroo.strippedMultiLineComments('This is a /*t\ne/*s*/\n/*t*/\n*/ of the JavaScript RegExp object')
+        assert newString == 'This is a \n of the JavaScript RegExp object'
+        pass
+    def testMultipleNestedMultiLine(self):
+        testaroo = CountLines()
+        newString = testaroo.strippedMultiLineComments('This is a /*t\ne/*s*/\n/*t*/\n*/ of the Java/*Script*/ RegExp object')
+        assert newString == 'This is a \n of the Java RegExp object'
+        pass
+    def testEndLineComments(self):
+        testaroo = CountLines()
+        newString = testaroo.stripOutLineComments('This is a //test of the JavaScript RegExp object')
+        assert newString == 'This is a '
+        pass
+    def testStripOutEmptyLines(self):
+        testaroo = CountLines()
+        newString = testaroo.stripOutBlankLines('test\r  \t  \na')
+        assert newString == 'test\na'
+        pass
+    def testJavaFileForLineCount(self):
+        f = open('../asantoro/fourlinefile.java')
+        javaFile = f.read()
+        testaroo = CountLines()
+        lineCount = testaroo.numberOfLines(javaFile)
+        assert lineCount == 4
+        pass
+    def testJavaEmptyBracesFileCount(self):
+        f = open('../asantoro/emptybraces.java')
+        javaFile = f.read()
+        testaroo = CountLines()
+        lineCount = testaroo.numberOfLines(javaFile)
+        assert lineCount == 6
+        pass
+    def testJavaHasBlankSpaceFileCount(self):
+        f = open('../asantoro/hasblankspace.java')
+        javaFile = f.read()
+        testaroo = CountLines()
+        lineCount = testaroo.numberOfLines(javaFile)
+        assert lineCount == 3
+        pass
+    def testJavaMultiLineCommentsFileCount(self):
+        f = open('../asantoro/multilinecomments.java')
+        javaFile = f.read()
+        testaroo = CountLines()
+        lineCount = testaroo.numberOfLines(javaFile)
+        assert lineCount == 5
+        pass
+    def testJavaThreeLineFileCount(self):
+        f = open('../asantoro/threelinefile.java')
+        javaFile = f.read()
+        testaroo = CountLines()
+        lineCount = testaroo.numberOfLines(javaFile)
+        assert lineCount == 4
+        pass
+    def testJavaThreeLinesAndCommentFileCount(self):
+        f = open('../asantoro/threelinesandcomment.java')
+        javaFile = f.read()
+        testaroo = CountLines()
+        lineCount = testaroo.numberOfLines(javaFile)
+        assert lineCount == 3
+        pass
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
