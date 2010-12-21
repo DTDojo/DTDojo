b61af9f66e89ef074abd806c3542cd59b3421501
tristopher <tris@triswarkentin.com>
2010-12-20 21:17:32 -0800

    updated it a little bit for more fun and better error messages =)

--- src/kata11/twarkentin/tris_new_solution.rb
+++ src/kata11/twarkentin/tris_new_solution.rb
@@ -1,8 +1,29 @@
+=begin
+Is word1 1 away from final_word2?
+	yes
+		put final_word2 on the end of our final_array, return it
+	no
+		find intersection of word1 and word2
+
+Is intersection > 0?
+	yes
+		1) is the intersection 1 away from word1?
+		yes
+			push intersection to final_array, run with intersection as new word_1
+		no
+			run again with intersection as word_2
+
+	no
+		expand size to include words connected to all present words
+		run again
+		
+=end
+
 require 'yaml'
 $word_map = YAML::load (File.read 'map.txt')
 dict_array = (File.read 'C:\Documents and Settings\twarkent\Desktop\programs\wordgame_kata\real_dictionary.txt').split("\n")
 
-final_array = []
+
 def connector_finder(word1,word2,final_word2,final_array)
 	final_array = [word1] if final_array == []
 	word1_array = [word1]
@@ -33,7 +54,7 @@ def connector_finder(word1,word2,final_word2,final_array)
 			word1_array = word1_array - [word1]
 			word2_array = word2_array - [word2]
 			error_count += 1
-			
+			#puts word2_array.length.to_s
 			if word1_array.length == word1_count 
 				puts word1_array.to_s
 				return "Nope, this one ain't solvable (one of your words is a dead end -- its whole tree group is listed above)"
@@ -72,15 +93,47 @@ def connector_finder(word1,word2,final_word2,final_array)
 end
 
 
+final_array = []
 entry = ''
-while entry == ''
-	rand1 = dict_array[rand(dict_array.length - 1)]
-	rand2 = dict_array[rand(dict_array.length - 1)]
-	if rand1.length == rand2.length
-		puts "OK, #{rand1} and #{rand2}..."
-		puts connector_finder(rand1,rand2,rand2,final_array)
+while true
+	if entry == ''
+		rand1 = dict_array[rand(dict_array.length - 1)]
+		rand2 = dict_array[rand(dict_array.length - 1)]
+		if rand1.length == rand2.length  #&& rand1.length < 4
+			puts "OK, #{rand1} and #{rand2}..."
+			puts connector_finder(rand1,rand2,rand2,final_array)
+			puts
+			puts "Again? (Press Enter to do another, any 'word1 word2' pair under 6 letters to specify your words, or 'exit' to exit)"
+			entry = gets.chomp
+		end
+	elsif entry == 'exit'
 		puts
-		puts "Again? (Press Enter to do another, or any other entry to exit)"
-		entry = gets.chomp
+		puts "OK!  Bye =)"
+		exit
+	else 
+		if entry.split(' ')[0] == nil || entry.split(' ')[1] == nil
+			puts "That's not the right format, doofus.  Try again."
+			entry = gets.chomp
+		else
+			word1 = entry.split(' ')[0]
+			word2 = entry.split(' ')[1]
+		
+			if (dict_array.include?(word1) == false || dict_array.include?(word2) == false)
+				puts "One'o'those ain't a word..."
+				entry = gets.chomp
+			elsif word1.length != word2.length
+				puts "They have to be the same length, doofus." 
+				entry = gets.chomp
+			elsif word1.length > 5 || word2.length > 5
+				puts "I said less than 6..."
+				entry = gets.chomp
+			else 
+				puts "OK, #{word1} and #{word2}..."
+				puts connector_finder(word1,word2,word2,final_array)
+				puts
+				puts "Again? (Press Enter to do another, any 'word1 word2' pair to specify your words, or 'exit' to exit)"
+				entry = gets.chomp
+			end
+		end	
 	end
 end
