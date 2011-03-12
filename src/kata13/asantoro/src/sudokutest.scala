70b3e0b46997b29441c9bbe9502597b56261ed0b
Alex S <santoro63@yahoo.com>
2011-03-11 16:07:47 -0800

    rakefile that works for compilation / unit tests

---
+++ src/kata13/asantoro/src/sudokutest.scala
@@ -0,0 +1,15 @@
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.junit.ShouldMatchersForJUnit
+import org.junit.Assert._
+import org.junit.Test
+
+class SquareTest extends JUnitSuite with ShouldMatchersForJUnit {
+
+  @Test def verifyOneElementSums() = {
+     println("done!!!!")
+  }
+
+  @Test def verifyTwoElementSums() = {
+    println("done too!!!")
+  }
+}
